9ae022f9fc3666f9347b691c4dfa8f1a5df2c28f
unknown <GeniousYash>
2022-12-18 15:07:18 +0530

    main page

---
+++ js/gsapindex.js
@@ -0,0 +1,42 @@
+gsap.from(".logo",{
+    scrollTrigger : {
+        scrub: true
+    },
+    x: -200,
+})
+gsap.from(".about_box",{
+    scrollTrigger : {
+        scrub: true
+    },
+    x: 300,
+})
+gsap.from(".myimgabout",{
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 500,
+})
+gsap.from(".contact_text",{
+    scrollTrigger : {
+        scrub: true
+    },
+    x: -500,
+})
+gsap.from(".wrapper",{
+    scrollTrigger : {
+        scrub: true
+    },
+    y: -1100,
+})
+gsap.from(".wrapper",{
+    scrollTrigger : {
+        scrub: true
+    },
+    x: 500,
+})
+gsap.from(".moveaboutmeheading",{
+    scrollTrigger : {
+        scrub: true
+    },
+    x: 1000,
+})
